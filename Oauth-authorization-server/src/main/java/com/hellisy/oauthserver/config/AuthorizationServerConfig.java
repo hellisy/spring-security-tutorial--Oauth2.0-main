d2eb6a8e4e9dd9536a6b2105db1630be6d4285e4
hellisy <heshamellisy1992@gmail.com>
2022-04-06 19:43:55 -0700

    Oauth2.0 w/SpringBoot
    Initial Commit

---
+++ Oauth-authorization-server/src/main/java/com/hellisy/oauthserver/config/AuthorizationServerConfig.java
@@ -0,0 +1,28 @@
+package com.hellisy.oauthserver.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configuration.OAuth;
+
+@Configuration(proxyBeanMethods = false)
+public class AuthorizationServerConfig {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    public SecurityFilterChain authServerSecurityFilterChain(HttpSecurity http) throws Exception {
+        OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
+
+        return http.formLogin(Customizer.withDefaults()).build();
+    }
+
+}
---
+++ Oauth-authorization-server/src/main/java/com/hellisy/oauthserver/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.hellisy.oauthserver.repository;
+
+
+import com.hellisy.oauthserver.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+    User findByEmail(String email);
+}
